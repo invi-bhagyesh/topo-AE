cd7225b720e452a6680f5292a6c469fafb3aa6b7
invi_bhagyesh <104337027+invi-bhagyesh@users.noreply.github.com>
2025-08-26 21:05:59 +0530

    changes for topo fawa training with size 44*28

--- src/datasets/syn.py
+++ src/datasets/syn.py
@@ -12,7 +12,7 @@ import torch
 #     os.path.join(os.path.dirname(__file__), '..', '..', 'src','datasets', 'data', 'characters'))
 
 #change for differnt dataset
-BASEPATH = "/kaggle/input/topo-training-data/train_gen_split/train_gen_split"
+BASEPATH = "/kaggle/input/test-adv-splitted/train_split"
 
 class SYN(Dataset):
     """Synthetic dataset with images and labels in filenames."""
