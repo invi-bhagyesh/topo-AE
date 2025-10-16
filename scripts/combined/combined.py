424f931827ef242ff3724d43d50b917cc8414592
invi_bhagyesh <104337027+invi-bhagyesh@users.noreply.github.com>
2025-10-16 19:38:24 +0530

    reformer commented out

--- scripts/combined/combined.py
+++ scripts/combined/combined.py
@@ -97,7 +97,7 @@ if __name__ == "__main__":
     # latent_nn = LatentNet()
     # latent_nn.load_state_dict(torch.load(latent_nn_path, map_location=device))
 
-    classifier_path = args.classifier_path if args.classifier_path is not None else f'./models/{dataset_name}_classifier.pth'
+    classifier_path = args.classifier_path # if args.classifier_path is not None else f'./models/{dataset_name}_classifier.pth'
     classifier = MNIST_CNN()
     classifier.load_state_dict(torch.load(classifier_path, map_location=device))
 
