468ade0914fb2083e59574d63e7f82589357e2d6
invi_bhagyesh <104337027+invi-bhagyesh@users.noreply.github.com>
2025-10-17 17:00:34 +0530

    import error

--- scripts/combined/combined.py
+++ scripts/combined/combined.py
@@ -5,7 +5,7 @@ import sys
 import os
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 sys.path.insert(0, parent_dir)
-from reformer import LatentReformer, LatentNet, MNIST_CNN
+from .reformer import LatentReformer, LatentNet, MNIST_CNN
 from src.models.approx_based import TopologicallyRegularizedAutoencoder
 
 
