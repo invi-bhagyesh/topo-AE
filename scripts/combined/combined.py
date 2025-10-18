ef1cae365d6ee981f000a0f9e6a0117edd6de96d
invi_bhagyesh <104337027+invi-bhagyesh@users.noreply.github.com>
2025-10-19 00:14:20 +0530

    last commit reverted

--- scripts/combined/combined.py
+++ scripts/combined/combined.py
@@ -23,7 +23,7 @@ class FullTopoPipeline(nn.Module):
         
         topo_img = self.topo_model.decode(latent)
         
-        topo_img = topo_img.sigmoid()  # First clamp to [0, 1]
+        topo_img = torch.clamp(topo_img, 0, 1)  # First clamp to [0, 1]
         topo_img = (topo_img - 0.5) / 0.5  # Then normalize to [-1, 1]
 
         # latent_out = self.latent_nn(latent)
