c161b7f8bcf4c928645db02f9a459cb553e1c852
invi_bhagyesh <104337027+invi-bhagyesh@users.noreply.github.com>
2025-10-18 15:15:56 +0530

    import error

--- watermark/STR_modules/ocr.py
+++ watermark/STR_modules/ocr.py
@@ -1,5 +1,6 @@
 import random
-import sys os
+import sys 
+import os
 sys.path.append('/Users/invi/Desktop/TopoReformer/TopoReformer/watermark')
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import time
