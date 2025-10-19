36cad7edb333c8707fb3e8f18e6a13b0a67fbc8f
invi_bhagyesh <104337027+invi-bhagyesh@users.noreply.github.com>
2025-10-20 02:33:39 +0530

    topo size to 128

--- scripts/combined/combined.py
+++ scripts/combined/combined.py
@@ -23,8 +23,8 @@ class FullTopoPipeline(nn.Module):
         
         topo_img = self.topo_model.decode(latent)
         
-        topo_img = torch.clamp(topo_img, 0, 1)  # First clamp to [0, 1]
-        topo_img = (topo_img - 0.5) / 0.5  # Then normalize to [-1, 1]
+        # topo_img = torch.clamp(topo_img, 0, 1)  # First clamp to [0, 1]
+        # topo_img = (topo_img - 0.5) / 0.5  # Then normalize to [-1, 1]
 
         # latent_out = self.latent_nn(latent)
         # print("latent OUT min:", latent_out.min().item(), "max:", latent_out.max().item())
