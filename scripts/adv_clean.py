364f483bf3360c54e0befbbd60a4c740893a7ad4
invi_bhagyesh <104337027+invi-bhagyesh@users.noreply.github.com>
2025-10-27 21:37:36 +0530

    fix clean topo

--- scripts/adv_clean.py
+++ scripts/adv_clean.py
@@ -15,94 +15,94 @@ from typing import Dict, Tuple
 import torch.nn as nn
 
 
-class AutoencoderModel(nn.Module, metaclass=abc.ABCMeta):
-    """Abstract base class for autoencoders."""
+# class AutoencoderModel(nn.Module, metaclass=abc.ABCMeta):
+#     """Abstract base class for autoencoders."""
 
-    # pylint: disable=W0221
-    @abc.abstractmethod
-    def forward(self, x) -> Tuple[float, Dict[str, float]]:
-        """Compute loss for model.
+#     # pylint: disable=W0221
+#     @abc.abstractmethod
+#     def forward(self, x) -> Tuple[float, Dict[str, float]]:
+#         """Compute loss for model.
 
-        Args:
-            x: Tensor with data
+#         Args:
+#             x: Tensor with data
 
-        Returns:
-            Tuple[loss, dict(loss_component_name -> loss_component)]
+#         Returns:
+#             Tuple[loss, dict(loss_component_name -> loss_component)]
 
-        """
+#         """
 
-    @abc.abstractmethod
-    def encode(self, x):
-        """Compute latent representation."""
+#     @abc.abstractmethod
+#     def encode(self, x):
+#         """Compute latent representation."""
 
-    @abc.abstractmethod
-    def decode(self, z):
-        """Compute reconstruction."""
-class View(nn.Module):
-    def __init__(self, shape):
-        super().__init__()
-        self.shape = shape
+#     @abc.abstractmethod
+#     def decode(self, z):
+#         """Compute reconstruction."""
+# class View(nn.Module):
+#     def __init__(self, shape):
+#         super().__init__()
+#         self.shape = shape
 
-    def forward(self, x):
-        return x.view(*self.shape)
+#     def forward(self, x):
+#         return x.view(*self.shape)
 
-class DeepAE(AutoencoderModel):
-    """1000-500-250-2-250-500-1000."""
-    def __init__(self, input_dims=(1, 28, 28)):
-        super().__init__()
-        self.input_dims = input_dims
-        n_input_dims = np.prod(input_dims)
-        self.encoder = nn.Sequential(
-            View((-1, n_input_dims)),
-            nn.Linear(n_input_dims, 1000),
-            nn.ReLU(True),
-            nn.BatchNorm1d(1000),
-            nn.Linear(1000, 500),
-            nn.ReLU(True),
-            nn.BatchNorm1d(500),
-            nn.Linear(500, 250),
-            nn.ReLU(True),
-            nn.BatchNorm1d(250),
-            nn.Linear(250, 2) # latent dim
-        )
-        self.decoder = nn.Sequential(
-            nn.Linear(2, 250), # latent dim
-            nn.ReLU(True),
-            nn.BatchNorm1d(250),
-            nn.Linear(250, 500),
-            nn.ReLU(True),
-            nn.BatchNorm1d(500),
-            nn.Linear(500, 1000),
-            nn.ReLU(True),
-            nn.BatchNorm1d(1000),
-            nn.Linear(1000, n_input_dims),
-            View((-1,) + tuple(input_dims)),
-            nn.Tanh()
-        )
-        self.reconst_error = nn.MSELoss()
+# class DeepAE(AutoencoderModel):
+#     """1000-500-250-2-250-500-1000."""
+#     def __init__(self, input_dims=(1, 28, 28)):
+#         super().__init__()
+#         self.input_dims = input_dims
+#         n_input_dims = np.prod(input_dims)
+#         self.encoder = nn.Sequential(
+#             View((-1, n_input_dims)),
+#             nn.Linear(n_input_dims, 1000),
+#             nn.ReLU(True),
+#             nn.BatchNorm1d(1000),
+#             nn.Linear(1000, 500),
+#             nn.ReLU(True),
+#             nn.BatchNorm1d(500),
+#             nn.Linear(500, 250),
+#             nn.ReLU(True),
+#             nn.BatchNorm1d(250),
+#             nn.Linear(250, 2) # latent dim
+#         )
+#         self.decoder = nn.Sequential(
+#             nn.Linear(2, 250), # latent dim
+#             nn.ReLU(True),
+#             nn.BatchNorm1d(250),
+#             nn.Linear(250, 500),
+#             nn.ReLU(True),
+#             nn.BatchNorm1d(500),
+#             nn.Linear(500, 1000),
+#             nn.ReLU(True),
+#             nn.BatchNorm1d(1000),
+#             nn.Linear(1000, n_input_dims),
+#             View((-1,) + tuple(input_dims)),
+#             nn.Tanh()
+#         )
+#         self.reconst_error = nn.MSELoss()
 
-    def encode(self, x):
-        """Compute latent representation using convolutional autoencoder."""
-        return self.encoder(x)
+#     def encode(self, x):
+#         """Compute latent representation using convolutional autoencoder."""
+#         return self.encoder(x)
 
-    def decode(self, z):
-        """Compute reconstruction using convolutional autoencoder."""
-        return self.decoder(z)
+#     def decode(self, z):
+#         """Compute reconstruction using convolutional autoencoder."""
+#         return self.decoder(z)
 
-    def forward(self, x):
-        """Apply autoencoder to batch of input images.
+#     def forward(self, x):
+#         """Apply autoencoder to batch of input images.
 
-        Args:
-            x: Batch of images with shape [bs x channels x n_row x n_col]
+#         Args:
+#             x: Batch of images with shape [bs x channels x n_row x n_col]
 
-        Returns:
-            tuple(reconstruction_error, dict(other errors))
+#         Returns:
+#             tuple(reconstruction_error, dict(other errors))
 
-        """
-        latent = self.encode(x)
-        x_reconst = self.decode(latent)
-        reconst_error = self.reconst_error(x, x_reconst)
-        return reconst_error, {'reconstruction_error': reconst_error}
+#         """
+#         latent = self.encode(x)
+#         x_reconst = self.decode(latent)
+#         reconst_error = self.reconst_error(x, x_reconst)
+#         return reconst_error, {'reconstruction_error': reconst_error}
 
 # Autoencoder definition (same as training)
 class Autoencoder(nn.Module):
